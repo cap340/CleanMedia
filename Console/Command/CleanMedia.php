dfd031513c68caed31c18ec76ec120708e9d40f7
cap340 <spicard@cap340.fr>
2019-11-14 05:24:36 +0100

    fix double // in $imageDir

--- Console/Command/CleanMedia.php
+++ Console/Command/CleanMedia.php
@@ -4,12 +4,15 @@ namespace Cap\CleanMedia\Console\Command;
 
 use Magento\Framework\App\Filesystem\DirectoryList;
 use Magento\Framework\App\ObjectManager;
+use RecursiveDirectoryIterator;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class CleanMedia extends Command
 {
+    protected $countFiles = 0;
+
     /**
      * @inheritDoc
      */
@@ -29,12 +32,21 @@ class CleanMedia extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $objectManager = ObjectManager::getInstance();
-        $filesystem    = $objectManager->get('Magento\Framework\Filesystem');
-        $directory     = $filesystem->getDirectoryRead(DirectoryList::MEDIA);
-        $imageDir      = $directory->getAbsolutePath().DIRECTORY_SEPARATOR.'catalog'.DIRECTORY_SEPARATOR.'product';
+        $objectManager     = ObjectManager::getInstance();
+        $filesystem        = $objectManager->get('Magento\Framework\Filesystem');
+        $directory         = $filesystem->getDirectoryRead(DirectoryList::MEDIA);
+        $imageDir          = $directory->getAbsolutePath().'catalog'.DIRECTORY_SEPARATOR.'product';
+        $directoryIterator = new RecursiveDirectoryIterator($imageDir);
 
-        $output->writeln('<info>Success Message.</info>');
-        $output->writeln('<error>An error encountered.</error>');
+        foreach (new \RecursiveIteratorIterator($directoryIterator) as $file) {
+            if (is_dir($file)) {
+                continue;
+            }
+            echo $file;
+            echo PHP_EOL;
+            $this->countFiles++;
+            echo $this->countFiles;
+            echo PHP_EOL;
+        }
     }
 }

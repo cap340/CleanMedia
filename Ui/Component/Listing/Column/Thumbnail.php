2dee0f62a31068b2558c1e72bc02e06aa2c20c52
cap340 <spicard@cap340.fr>
2020-01-23 11:28:28 +0100

    Ui listing component: add Thumbnail.php & columns

---
+++ Ui/Component/Listing/Column/Thumbnail.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Cap\CleanMedia\Ui\Component\Listing\Column;
+
+use Magento\Framework\UrlInterface;
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Framework\View\Element\UiComponentFactory;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\Ui\Component\Listing\Columns\Column;
+
+class Thumbnail extends Column
+{
+    const NAME = 'image';
+    const ALT_FIELD = 'name';
+
+    /**
+     * @var StoreManagerInterface
+     */
+    protected $_storeManager;
+
+    /**
+     * @var UrlInterface
+     */
+    protected $urlBuilder;
+
+    /**
+     * Thumbnail constructor.
+     *
+     * @param ContextInterface $context
+     * @param UiComponentFactory $uiComponentFactory
+     * @param UrlInterface $urlBuilder
+     * @param StoreManagerInterface $storeManager
+     * @param array $components
+     * @param array $data
+     */
+    public function __construct(
+        ContextInterface $context,
+        UiComponentFactory $uiComponentFactory,
+        UrlInterface $urlBuilder,
+        StoreManagerInterface $storeManager,
+        array $components = [],
+        array $data = []
+    ) {
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+        $this->_storeManager = $storeManager;
+        $this->urlBuilder = $urlBuilder;
+    }
+
+    /**
+     * Prepare Data Source
+     *
+     * @param array $dataSource
+     * @return array
+     */
+    public function prepareDataSource(array $dataSource)
+    {
+        if (isset($dataSource['data']['items'])) {
+            $fieldName = $this->getData('name');
+            foreach ($dataSource['data']['items'] as & $item) {
+                $mediaRelativePath = $this->_storeManager->getStore()->getBaseUrl(UrlInterface::URL_TYPE_MEDIA);
+                $logoPath = $mediaRelativePath . $item['relativePath'];
+                $item[$fieldName . '_src'] = $logoPath;
+                $item[$fieldName . '_alt'] = $this->getAlt($item);
+                $item[$fieldName . '_orig_src'] = $logoPath;
+            }
+        }
+
+        return $dataSource;
+    }
+
+    /**
+     * @param array $row
+     *
+     * @return null|string
+     */
+    protected function getAlt($row)
+    {
+        $altField = self::ALT_FIELD;
+        return isset($row[$altField]) ? $row[$altField] : null;
+    }
+}

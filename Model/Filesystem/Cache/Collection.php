e52898133fb6b5f8358b3db38bf5729c1fe08e4b
cap340 <spicard@cap340.fr>
2020-01-26 19:03:13 +0100

    Cache collection & remove underscore @var

--- Model/Filesystem/Cache/Collection.php
+++ Model/Filesystem/Cache/Collection.php
@@ -1,8 +1,7 @@
 <?php
 
-namespace Cap\CleanMedia\Model\Filesystem;
+namespace Cap\CleanMedia\Model\Filesystem\Cache;
 
-use DateTime;
 use Exception;
 use Magento\Framework\App\Filesystem\DirectoryList;
 use Magento\Framework\Data\Collection\EntityFactoryInterface;
@@ -10,19 +9,8 @@ use Magento\Framework\Exception\FileSystemException;
 use Magento\Framework\Filesystem;
 use Magento\Framework\Filesystem\Directory\WriteInterface;
 
-/**
- * Filesystem collection
- *
- */
 class Collection extends \Magento\Framework\Data\Collection\Filesystem
 {
-    /**
-     * Exclude cache folder
-     *
-     * @var string
-     */
-    protected $_allowedDirsMask = '/^(?!cache|(?!([^.]))).*/';
-
     /**
      * @var WriteInterface
      */
@@ -33,7 +21,7 @@ class Collection extends \Magento\Framework\Data\Collection\Filesystem
      *
      * @var string
      */
-    protected $path = 'catalog/product';
+    protected $path = 'catalog/product/cache';
 
     /**
      * @var Filesystem
@@ -61,18 +49,6 @@ class Collection extends \Magento\Framework\Data\Collection\Filesystem
     }
 
     /**
-     * Add order to sort the collection in listing ui_component
-     *
-     * @param $field
-     * @param $direction
-     * @return \Magento\Framework\Data\Collection
-     */
-    public function addOrder($field, $direction)
-    {
-        return $this->setOrder($field, $direction);
-    }
-
-    /**
      * Generate item row basing on the filename
      *
      * Keep only 'basename', rest could be customized
@@ -83,15 +59,9 @@ class Collection extends \Magento\Framework\Data\Collection\Filesystem
      */
     protected function _generateRow($filename)
     {
-        $ctime = $this->mediaDirectory->stat($this->mediaDirectory->getRelativePath($filename))['ctime'];
-        $upload_date = DateTime::createFromFormat("U", $ctime)->format('Y-m-d H:i:s');
-        //todo: fix The use of function basename() is discouraged
         return [
             'path' => $filename,
             'basename' => basename($filename),
-            'size' => $this->mediaDirectory->stat($this->mediaDirectory->getRelativePath($filename))['size'],
-            'upload' => $upload_date,
-            'relativePath' => $this->mediaDirectory->getRelativePath($filename)
         ];
     }
 }

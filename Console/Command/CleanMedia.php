7dd4b4a63f49d167ad1cd2aaeb1a8af541cfa331
cap340 <spicard@cap340.fr>
2020-01-30 13:55:44 +0100

    iterate $mediaPath

--- Console/Command/CleanMedia.php
+++ Console/Command/CleanMedia.php
@@ -2,12 +2,52 @@
 
 namespace Cap\CleanMedia\Console\Command;
 
+use FilesystemIterator;
+use Magento\Framework\App\Filesystem\DirectoryList;
+use Magento\Framework\Filesystem;
+use Magento\Framework\Filesystem\Driver\File;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+use SplFileInfo;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class CleanMedia extends Command
 {
+    /**
+     * Folder, where all media are stored
+     *
+     * @var string
+     */
+    protected $path = 'catalog/product';
+
+    /**
+     * @var Filesystem\Directory\ReadInterface
+     */
+    protected $mediaDirectory;
+
+    /**
+     * @var File
+     */
+    protected $driverFile;
+
+    /**
+     * CleanMedia constructor.
+     *
+     * @param Db $resourceDb
+     * @param Filesystem $filesystem
+     * @param File $driverFile
+     */
+    public function __construct(
+        Filesystem $filesystem,
+        File $driverFile
+    ) {
+        parent::__construct();
+        $this->mediaDirectory = $filesystem->getDirectoryRead(DirectoryList::MEDIA);
+        $this->driverFile = $driverFile;
+    }
+
     /**
      * @inheritDoc
      */
@@ -25,6 +65,17 @@ class CleanMedia extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln('clean media');
+        $mediaPath = $this->mediaDirectory->getAbsolutePath() . $this->path;
+        $iterator = new RecursiveIteratorIterator(
+            new RecursiveDirectoryIterator(
+                $mediaPath,
+                FilesystemIterator::SKIP_DOTS | FilesystemIterator::FOLLOW_SYMLINKS
+            )
+        );
+
+        /** @var SplFileInfo $file */
+        foreach ($iterator as $file) {
+            $output->writeln($file->getFilename());
+        }
     }
 }
